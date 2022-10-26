9fbbc239a05b08fb785bc3b9b765bfb6d77de193
Barthh <barthelemy.popille@epfedu.fr>
2022-10-26 12:37:23 +0200

    Put url in variable

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 # main.py
 import package
 
-qr_code = package.QrCodeGenerator.generateQrCode("https://jardinage.lemonde.fr/images/dossiers/2017-09/castor-113620.jpg")
+url = "https://jardinage.lemonde.fr/images/dossiers/2017-09/castor-113620.jpg"
+
+# Get qr code
+qr_code = package.QrCodeGenerator.generateQrCode(url)
+
+# Save and print output
 qr_code.img.save('generated_qr_code.png')
 print(qr_code.url)
