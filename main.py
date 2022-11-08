bbe0adb4b85631ffcc65e7ec93d33dd13bfbec10
Barthh <barthelemy.popille@epfedu.fr>
2022-11-08 07:09:28 +0100

    Add title, subtitle and no refund text

--- main.py
+++ main.py
@@ -6,7 +6,12 @@ url = "https://google.fr"
 amaztemp = package.TicketGenerator(
     message = 'Hello, world!',
     url = url,
+    title = "This is the title",
+    subtitle = "This is the subtitle",
 )
 
 img = amaztemp.generateTicket()
 img.show()
+
+# # new_image.save("images/merged_image.jpg","PNG")
+# new_image.show()
--- package/ticker_generator.py
+++ package/ticker_generator.py
@@ -3,9 +3,11 @@ from PIL import Image, ImageDraw, ImageFont #Import PIL functions
 
 class TicketGenerator:
 
-    def __init__(self, message, url):
+    def __init__(self, message: str, url: str, title: str, subtitle: str):
         self.message = message # Little message
         self.url = url # Qr code image
+        self.title = str(title).upper()
+        self.subtitle = str(subtitle).upper()
 
     def generateTicket(self):
 
@@ -14,10 +16,10 @@ class TicketGenerator:
         qr_code = qr_code.resize((360, 360))
 
         # Background image
-        background = Image.open('./Images/template/background.png', 'r').convert('RGB') #Opens Template Image
+        background = Image.open('./ressources/template/background.png', 'r').convert('RGB') #Opens Template Image
         
         # At the end, this picture should be load randomly :
-        special_picture = Image.open('./Images/football/DALL·E 2022-11-08 01.21.01 - A cyberpunk representation of soccer goal.png','r').convert('RGB')
+        special_picture = Image.open('./ressources/football/DALL·E 2022-11-08 01.21.01 - A cyberpunk representation of soccer goal.png','r').convert('RGB')
         special_picture = special_picture.resize((630, 630))
 
         # Add pictures on the background 
@@ -25,12 +27,23 @@ class TicketGenerator:
         background.paste(special_picture, (1947, 120))
 
 
+        # Il faudra mettre une limite de caractère maximale
 
-        # Call draw Method to add 2D graphics in an image
-        I1 = ImageDraw.Draw(background)
+        def addNewDraw(text, width, heigth, fontsize, fontcolor, fontwidth:int = 1):
+            draw = ImageDraw.Draw(background)
+            myFont = ImageFont.truetype("./ressources/courrier_new.ttf", fontsize)
+            w, h = draw.textsize(text, font = myFont)
+            draw.text((width - (w/2), heigth - (h/2)), text, font= myFont, stroke_width=fontwidth, fill=fontcolor)
 
-        # Add Text to an image
-        I1.text((28, 36), self.message, fill=(255, 0, 0))
+        # Display title
+        color = (30, 61, 89)
+        addNewDraw(self.title, 1220, 320, 120, color, 3)
+       
+        # Display subtitle
+        addNewDraw(self.subtitle, 1220, 500, 90, color, 3)
 
+        # Display no refund
+        text = "No refund / No exchange"
+        addNewDraw(text, 815, 815, 38, (245, 240, 225))
 
         return background
