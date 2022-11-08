d82d9f98452fb0418793d95a5d9432fec331a332
Barthh <barthelemy.popille@epfedu.fr>
2022-11-08 12:35:31 +0100

    Add input parameters

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 # main.py
 import package
+import os, random, glob
 
-url = "https://google.fr"
+url = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+picture_path = random.choice(glob.glob("ressources/football/*.png"))
+print(picture_path)
 
 amaztemp = package.TicketGenerator(
     message = 'Hello, world!',
     url = url,
-    title = "This is the title",
-    subtitle = "This is the subtitle",
+    title = "Uefa euro 2024",
+    subtitle = "Finale",
+    date = "23/08/2022 21:00:00",
+    picture = picture_path,
 )
 
 img = amaztemp.generateTicket()
