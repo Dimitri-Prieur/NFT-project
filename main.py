f3530ef0e4725d957b85b2fa217b053e91ba1063
Barthh <barthelemy.popille@epfedu.fr>
2022-11-29 08:43:08 +0100

    update

--- main.py
+++ main.py
@@ -3,22 +3,22 @@ import package
 import os, random, glob
 
 # Picture data
-_url = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+_url = "https://www.google.fr/"
+_ticket_id = 10
 _picture_path = random.choice(glob.glob("ressources/football/*.png"))
-_ticket_id = 1
 
-# Create self informations
+# Create self informations    
 informations = package.TicketGenerator(
     id = _ticket_id,
     url = _url,
-    price = 30.00,
+    price = 89.00,
     picture = _picture_path,
-    title = "Uefa euro 2024",
-    subtitle = "Finale",
-    date = "23/08/2022 21:00:00",
+    title = "UEFA Euro 2024",
+    subtitle = "FINALE",
+    date = "23/08/2024 21:00:00",
     wallet = "0xC841962098B5592A415493992cd7F521347632f7",
-    location = "Stade de France",
-    seat = '38A',
+    location = "Stade de France, Paris",
+    seat = "10D",
 )
 
 # Generate ticket image
@@ -31,10 +31,8 @@ json_data = informations.generate_json()
 # img.show()
 
 # Save ticket image
-img.save("generated_tickets/picture/" + str(_ticket_id) + ".png","PNG")
+img.save("generated_tickets/picture/football_" + str(_ticket_id) + ".png","PNG")
 
 # Save ticket json
-with open("generated_tickets/json/" + str(_ticket_id) + ".json", 'w') as outfile:
+with open("generated_tickets/json/football_" + str(_ticket_id) + ".json", 'w') as outfile:
     outfile.write(json_data)
-    
-print(json_data)
