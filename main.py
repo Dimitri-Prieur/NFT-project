6c69c4b2be7b0b99dbf11625972b6a289ed7f4c3
Barthh <barthelemy.popille@epfedu.fr>
2022-11-29 10:14:01 +0100

    Change readme picture

--- main.py
+++ main.py
@@ -4,21 +4,21 @@ import os, random, glob
 
 # Picture data
 _url = "https://www.google.fr/"
-_ticket_id = 10
+_ticket_id = 0
 _picture_path = random.choice(glob.glob("ressources/football/*.png"))
 
 # Create self informations    
 informations = package.TicketGenerator(
     id = _ticket_id,
     url = _url,
-    price = 89.00,
+    price = 10.00,
     picture = _picture_path,
-    title = "UEFA Euro 2024",
-    subtitle = "FINALE",
-    date = "23/08/2024 21:00:00",
+    title = "event title",
+    subtitle = "subtitle",
+    date = "01/01/2000 12:00:00",
     wallet = "0xC841962098B5592A415493992cd7F521347632f7",
-    location = "Stade de France, Paris",
-    seat = "10D",
+    location = "Here is the location",
+    seat = "XXX",
 )
 
 # Generate ticket image
@@ -31,8 +31,8 @@ json_data = informations.generate_json()
 # img.show()
 
 # Save ticket image
-img.save("generated_tickets/picture/football_" + str(_ticket_id) + ".png","PNG")
+img.save("generated_tickets/picture/test_" + str(_ticket_id) + ".png","PNG")
 
 # Save ticket json
-with open("generated_tickets/json/football_" + str(_ticket_id) + ".json", 'w') as outfile:
+with open("generated_tickets/json/test_" + str(_ticket_id) + ".json", 'w') as outfile:
     outfile.write(json_data)
